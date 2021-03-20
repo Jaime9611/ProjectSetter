6d29db6733f5e9bda8a605b9d68009558999c052
Jaime9611 <jaimeescobar9611@gmail.com>
2021-03-20 15:14:56 -0500

    Trying to pkg the projects

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='ProjectSetter',
     version='0.1',
     py_modules=['hello', 'folders', 'cli_commands'],
+    install_requires=[
+        'Click',
+    ],
+    include_package_data=True,
     entry_points='''
         [console_scripts]
         project-set=hello:cli
