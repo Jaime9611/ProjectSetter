3722b5edbc65c531d24df3310cd75e54d78310b8
Jaime9611 <jaimeescobar9611@gmail.com>
2021-08-10 17:33:02 -0500

    Added folders tests file

--- tests/context.py
+++ tests/context.py
@@ -5,3 +5,4 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from project_setter import data
 from project_setter import cli_commands
+from project_setter import folders
