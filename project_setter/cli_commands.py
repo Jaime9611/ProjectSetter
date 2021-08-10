f7f2979ee09ecb6cb2313637167d418f3afacf27
Jaime9611 <jaimeescobar9611@gmail.com>
2021-08-10 16:23:46 -0500

    Added git test to cli Tests

--- project_setter/cli_commands.py
+++ project_setter/cli_commands.py
@@ -1,17 +1,24 @@
 import subprocess
 import os
 import platform
+import pathlib
 
 
 def start_git(route):
+    assert isinstance(route, pathlib.Path), 'Parameter not a Path type.'
+
     call = subprocess.run(['git', 'init', route])
 
 
 def start_vscode(route):
+    assert isinstance(route, pathlib.Path), 'Parameter not a Path type.'
+
     call = subprocess.run(['code', route], shell=True)
 
 
 def show_dir_path(route):
+    assert isinstance(route, pathlib.Path), 'Parameter not a Path type.'
+
     result = subprocess.run(['cd', route, '&&', 'pwd'], 
         shell=True, capture_output=True).stdout.decode('utf-8')
     command = 'echo | set /p nul=' + result.strip() + '| clip'
@@ -24,5 +31,7 @@ def get_pwd_path():
 
     if result.startswith('/c/') and platform.system() == 'Windows':
         result = result.replace('/c/', 'C:/')
+
+    assert type(result) == str, 'Not returning a String'
 
     return result.strip()
--- tests/cli_commands_tests.py
+++ tests/cli_commands_tests.py
@@ -1,10 +1,31 @@
 import unittest
+import subprocess
+from pathlib import Path
+
 
 from .context import cli_commands
 
-
 class CliTest(unittest.TestCase):
-    pass
+    @classmethod
+    def setUpClass(cls):
+        cls.test_dir = Path(__file__).parent / 'TestDir'
+        cls.test_dir.mkdir()
+
+    def test_temporary_dir_created(self):
+        self.assertTrue(self.test_dir.exists())
+
+    def test_start_git_repository(self):
+        cli_commands.start_git(self.test_dir)
+
+        dir_files = self.test_dir.iterdir()
+        dir_files = [file.name for file in dir_files]
+        print(dir_files)
+
+        self.assertTrue('.git' in dir_files)
+
+    @classmethod
+    def tearDownClass(cls):
+        remove = subprocess.run(['rm', '-rf', cls.test_dir], shell=True)
 
 
 
