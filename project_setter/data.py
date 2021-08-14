a165cfdfb76f6a05eb712e89807006bb915d82c8
Jaime9611 <jaimeescobar9611@gmail.com>
2021-08-14 11:33:06 -0500

    Data Documentation

--- project_setter/data.py
+++ project_setter/data.py
@@ -1,3 +1,16 @@
+"""Data Script
+
+This script is used to access the .json resources with the path and folder structure data.
+
+This script has the following:
+    * PATHS - Variable with the project_paths.json location.
+    * STRUCTURES - Variable with the structures.json location.
+    * Project - Enum class with the project types' names.
+    * _get_data(source, project_type) - Helper function to obtain the json data.
+    * get_project_structure(project) - Function that returns the project's layout structure of the given project type(Enum.Project).
+    * get_base_path(project) - Function that returns the project's main folder path of the given project type(Enum.Project).
+"""
+
 import json
 import pkg_resources
 from enum import Enum
@@ -7,11 +20,35 @@ STRUCTURES = pkg_resources.resource_filename('project_setter', 'resources/struct
 
 # Variables
 class Project(Enum):
+    """Enum class with the names of the types of projects.
+    
+    Attributes
+    __________
+    WEB : String
+        Stores the name used for the web's project type.
+    PYTHON : String
+        Stores the name used for the python's project type.
+    """
+
     WEB = 'web'
     PYTHON = 'python'
 
 
 def _get_data(source, project_type):
+    """Helper method that returns the json data.
+
+    Parameters
+    __________
+    source : String Object
+        Receives the .json file location.
+    project_type : Enum.Project Object
+        Contains the project's type.
+
+    Returns
+    _______
+    String or Dict
+        String or dictionary containing the data extracted from the .json file.
+    """
 
     with open(source) as f:
         data = json.load(f)
@@ -22,12 +59,21 @@ def _get_data(source, project_type):
 
 
 def get_project_structure(project):
+    """Function that obtains the project's folder structure.
+
+    This function calls the helper function _get_data().
+    """
     assert isinstance(project, Project), 'Argument type not a Project variable.'
 
     return _get_data(STRUCTURES, project)
 
 
 def get_base_path(project):
+    """Method that returns the project's parent path.
+
+    This function calls the helper function _get_data().
+    """
+
     assert isinstance(project, Project), 'Argument type not a Project variable.'
 
     return _get_data(PATHS, project)
